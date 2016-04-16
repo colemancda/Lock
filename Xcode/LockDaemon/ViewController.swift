aeab0b5e349e09fa10352ac5d810de5491428075
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-04-16 02:00:59 -0500

    created Xcode project

---
+++ Xcode/LockDaemon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LockDaemon
+//
+//  Created by Alsey Coleman Miller on 4/16/16.
+//  Copyright © 2016 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
