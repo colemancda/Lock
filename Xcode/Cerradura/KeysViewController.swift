636d9d0e34977d8b7343acc3379a5419934c8a54
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-04-23 20:29:35 -0500

    added key deletion in app

--- Xcode/Cerradura/KeysViewController.swift
+++ Xcode/Cerradura/KeysViewController.swift
@@ -11,11 +11,22 @@ import UIKit
 import CoreData
 import CoreLock
 
-final class ViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+final class KeysViewController: UITableViewController /* NSFetchedResultsControllerDelegate */ {
     
     // MARK: - Properties
     
-    private lazy var fetchedResultsController: NSFetchedResultsController = NSFetchedResultsController(fetchRequest: NSFetchRequest(entityName: LockCache.entityName), managedObjectContext: Store.shared.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+    private lazy var fetchedResultsController: NSFetchedResultsController = {
+        
+        let fetchRequest = NSFetchRequest(entityName: LockCache.entityName)
+        
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: LockCache.Property.name.rawValue, ascending: true)]
+        
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: Store.shared.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        return controller
+    }()
+    
+    private lazy var controller: Controller = Controller(tableView: self.tableView, fetchedResultsController: self.fetchedResultsController)
     
     // MARK: - Loading
     
@@ -23,122 +34,173 @@ final class ViewController: UITableViewController, NSFetchedResultsControllerDel
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        fetchedResultsController.delegate = unsafeBitCast(self.controller, to: NSFetchedResultsControllerDelegate.self)
+        tableView.dataSource = unsafeBitCast(self.controller, to: UITableViewDataSource.self)
+        tableView.delegate = unsafeBitCast(self.controller, to: UITableViewDelegate.self)
+        
         try! fetchedResultsController.performFetch()
     }
+}
+
+private extension KeysViewController {
     
-    // MARK: - Private Methods
-    
-    private func configure(cell: KeyTableViewCell, at indexPath: NSIndexPath) {
+    @objc private final class Controller: NSObject {
         
-        let managedObject = fetchedResultsController.object(at: indexPath) as! NSManagedObject
+        weak var tableView: UITableView!
         
-        let lock = LockCache(managedObject: managedObject)
+        weak var fetchedResultsController: NSFetchedResultsController!
         
-        let permissionImage: UIImage
-        
-        let permissionText: String
-        
-        switch lock.permission {
+        private init(tableView: UITableView, fetchedResultsController: NSFetchedResultsController) {
             
-        case .owner:
-            
-            permissionImage = UIImage(named: "permissionBadgeOwner")!
-            
-            permissionText = "Owner"
-            
-        case .admin:
-            
-            permissionImage = UIImage(named: "permissionBadgeAdmin")!
-            
-            permissionText = "Admin"
-            
-        case .anytime:
-            
-            permissionImage = UIImage(named: "permissionBadgeAnytime")!
-            
-            permissionText = "Anytime"
-            
-        case let .scheduled(schedule):
-            
-            permissionImage = UIImage(named: "permissionBadgeScheduled")!
-            
-            permissionText = "Scheduled" // FIXME
+            self.tableView = tableView
+            self.fetchedResultsController = fetchedResultsController
         }
         
-        cell.lockNameLabel.text = lock.name
-        
-        cell.permissionImageView.image = permissionImage
-        
-        cell.permissionLabel.text = permissionText
-    }
-    
-    // MARK: - UITableViewDatasource
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return fetchedResultsController.fetchedObjects?.count ?? 0
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: KeyTableViewCell.resuseIdentifier, for: indexPath) as! KeyTableViewCell
-        
-        configure(cell: cell, at: indexPath)
-        
-        return cell
-    }
-    
-    // MARK: - NSFetchedResultsControllerDelegate
-    
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController) {
-        
-        tableView.beginUpdates()
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController) {
-        
-        tableView.endUpdates()
-    }
-    
-    func controller(_ controller: NSFetchedResultsController, didChange anObject: AnyObject, at indexPath: NSIndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-        
-        switch type {
+        private func item(at indexPath: NSIndexPath) -> LockCache {
             
-        case .insert:
+            let managedObject = fetchedResultsController.object(at: indexPath) as! NSManagedObject
             
-            if let newIndexPath = newIndexPath {
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
-            }
+            let lock = LockCache(managedObject: managedObject)
             
-        case .delete:
+            return lock
+        }
+        
+        private func configure(cell: KeyTableViewCell, at indexPath: NSIndexPath) {
             
-            if let indexPath = indexPath {
-                tableView.deleteRows(at: [indexPath], with: .automatic)
-            }
+            let lock = item(at: indexPath)
             
-        case .update:
+            let permissionImage: UIImage
             
-            if let indexPath = indexPath {
+            let permissionText: String
+            
+            switch lock.permission {
                 
-                if let cell = tableView.cellForRow(at: indexPath) as? KeyTableViewCell {
-                    
-                    self.configure(cell: cell, at: indexPath)
-                }
+            case .owner:
+                
+                permissionImage = UIImage(named: "permissionBadgeOwner")!
+                
+                permissionText = "Owner"
+                
+            case .admin:
+                
+                permissionImage = UIImage(named: "permissionBadgeAdmin")!
+                
+                permissionText = "Admin"
+                
+            case .anytime:
+                
+                permissionImage = UIImage(named: "permissionBadgeAnytime")!
+                
+                permissionText = "Anytime"
+                
+            case let .scheduled(schedule):
+                
+                permissionImage = UIImage(named: "permissionBadgeScheduled")!
+                
+                permissionText = "Scheduled" // FIXME
             }
             
-        case .move:
+            cell.lockNameLabel.text = lock.name
             
-            if let indexPath = indexPath {
+            cell.permissionImageView.image = permissionImage
+            
+            cell.permissionLabel.text = permissionText
+        }
+        
+        // MARK: - UITableViewDatasource
+        
+        @objc func numberOfSectionsInTableView(_ tableView: UITableView) -> Int {
+            
+            return 1
+        }
+        
+        @objc func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            return fetchedResultsController.fetchedObjects?.count ?? 0
+        }
+        
+        @objc func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: KeyTableViewCell.resuseIdentifier, for: indexPath) as! KeyTableViewCell
+            
+            configure(cell: cell, at: indexPath)
+            
+            return cell
+        }
+        
+        // MARK: - UITableViewDelegate
+        
+        /*
+        @objc func tableView(_ tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+            
+            let lock = item(at: indexPath)
+            
+            Store.shared.remove(lock.identifier)
+        }
+        
+        @objc func tableView(_ tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+            
+            return .delete
+        }*/
+        
+        func tableView(_ tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+            
+            
+        }
+        
+        // MARK: - NSFetchedResultsControllerDelegate
+        
+        @objc func controllerWillChangeContent(_ controller: NSFetchedResultsController) {
+            
+            tableView.beginUpdates()
+        }
+        
+        @objc func controllerDidChangeContent(_ controller: NSFetchedResultsController) {
+            
+            tableView.endUpdates()
+        }
+        
+        @objc func controller(_ controller: NSFetchedResultsController,
+                              didChangeObject anObject: AnyObject,
+                              atIndexPath indexPath: NSIndexPath?,
+                              forChangeType type: NSFetchedResultsChangeType,
+                              newIndexPath: NSIndexPath?) {
+            
+            sleep(1)
+            
+            switch type {
+                
+            case .insert:
                 
                 if let newIndexPath = newIndexPath {
-                    
-                    tableView.deleteRows(at: [indexPath], with: .automatic)
                     tableView.insertRows(at: [newIndexPath], with: .automatic)
+                }
+                
+            case .delete:
+                
+                if let indexPath = indexPath {
+                    tableView.deleteRows(at: [indexPath], with: .automatic)
+                }
+                
+            case .update:
+                
+                if let indexPath = indexPath {
+                    
+                    if let cell = tableView.cellForRow(at: indexPath) as? KeyTableViewCell {
+                        
+                        self.configure(cell: cell, at: indexPath)
+                    }
+                }
+                
+            case .move:
+                
+                if let indexPath = indexPath {
+                    
+                    if let newIndexPath = newIndexPath {
+                        
+                        tableView.deleteRows(at: [indexPath], with: .automatic)
+                        tableView.insertRows(at: [newIndexPath], with: .automatic)
+                    }
                 }
             }
         }
