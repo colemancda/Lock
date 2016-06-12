8af7ba79483483a1af79e06c39929421e4f34ff8
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-11 23:27:25 -0500

    refactored LockManager for non-persistent connections
    so the Lock can be used by multiple clients

--- Sources/CoreLock/LockManager.swift
+++ Sources/CoreLock/LockManager.swift
@@ -30,15 +30,9 @@
             set { internalManager.log = newValue }
         }
         
-        public let scanDuration = 2
-        
-        public let foundLock = Observable<(peripheral: Peripheral, UUID: SwiftFoundation.UUID, status: Status, model: Model, version: UInt64)?>()
-        
         public lazy var state: Observable<CBCentralManagerState> = Observable(self.internalManager.state)
         
         // MARK: - Private Properties
-        
-        private lazy var queue: dispatch_queue_t = dispatch_queue_create("\(self.dynamicType) Internal Queue", DISPATCH_QUEUE_SERIAL)
         
         private lazy var internalManager: CentralManager = {
             
@@ -47,56 +41,63 @@
             // lazy initialization for CBCentralManager
             let _ = central.state
             
-            central.stateChanged = self.stateChanged
-            central.didDisconnect = self.didDisconnect
+            central.stateChanged = { self.state.value = $0 }
             
             return central
         }()
         
         // MARK: - Methods
         
-        /// Disconnnect from current lock (if any) and start scanning. (Asyncronous)
-        public func startScan() {
+        /// Disconnnect from current lock (if any) and scans for a lock. 
+        ///
+        /// - Parameter duration: The duration of the scan.
+        ///
+        /// - Parameter filter: The UUID Lock to find, or `nil` for the first lock found.
+        ///
+        /// - Returns: The first lock found.
+        public func scan(duration: Int = 2, filter UUID: SwiftFoundation.UUID? = nil) throws -> Lock? {
+            
+            assert(self.internalManager.state == .poweredOn, "Should only scan when powered on")
             
             log?("Scanning...")
             
             internalManager.disconnectAll()
             
-            if foundLock.value != nil { foundLock.value = nil }
+            let foundDevices = self.internalManager.scan(duration: duration)
             
-            async {
+            if foundDevices.count > 0 { self.log?("Found \(foundDevices.count) peripherals") }
+            
+            for peripheral in foundDevices {
                 
-                while self.internalManager.state == .poweredOn && self.foundLock.value == nil {
+                do { try self.internalManager.connect(to: peripheral) }
                     
-                    let foundDevices = self.internalManager.scan(duration: self.scanDuration)
+                catch { print("Cound not connect to \(peripheral.identifier) (\(error))"); continue }
+                
+                guard let services = try? self.internalManager.discoverServices(for: peripheral)
+                    else { continue }
+                
+                // found lock
+                if services.contains({ $0.UUID == LockService.UUID }) {
                     
-                    if foundDevices.count > 0 { self.log?("Found \(foundDevices.count) peripherals") }
+                    let foundLock = try self.foundLock(peripheral: peripheral)
                     
-                    for peripheral in foundDevices {
-                        
-                        do { try self.internalManager.connect(to: peripheral) }
-                            
-                        catch { print("Cound not connect to \(peripheral.identifier) (\(error))"); continue }
-                        
-                        guard let services = try? self.internalManager.discoverServices(for: peripheral)
-                            else { continue }
-                        
-                        // found lock
-                        if services.contains({ $0.UUID == LockService.UUID }) {
-                            
-                            self.foundLock(peripheral: peripheral)
-                            return
-                        }
-                    }
+                    // optionally filter locks
+                    guard foundLock.UUID == UUID || UUID != nil else { continue }
+                    
+                    return foundLock
                 }
             }
+            
+            return nil
         }
         
-        /// Setup the connected lock
-        public func setup(name: String) throws -> Key {
+        public func disconnect(lock: Lock) {
             
-            guard let foundLock = self.foundLock.value
-                else { throw Error.NoLock }
+            internalManager.disconnect(peripheral: lock.peripheral)
+        }
+        
+        /// Setup the connected lock.
+        public func setup(lock: inout Lock) throws -> Key {
             
             // write to setup characteristic
             
@@ -104,11 +105,11 @@
             
             let setup = LockService.Setup.init(value: key.data)
             
-            try internalManager.write(data: setup.toBigEndian(), response: true, characteristic: LockService.Setup.UUID, service: LockService.UUID, peripheral: foundLock.peripheral)
+            try internalManager.write(data: setup.toBigEndian(), response: true, characteristic: LockService.Setup.UUID, service: LockService.UUID, peripheral: lock.peripheral)
             
             // read lock service values
             
-            let statusValue = try internalManager.read(characteristic: LockService.Status.UUID, service: LockService.UUID, peripheral: foundLock.peripheral)
+            let statusValue = try internalManager.read(characteristic: LockService.Status.UUID, service: LockService.UUID, peripheral: lock.peripheral)
             
             guard let status = LockService.Status.init(bigEndian: statusValue)
                 else { throw LockManagerError.InvalidCharacteristicValue(LockService.Status.UUID) }
@@ -117,51 +118,36 @@
                 else { throw LockManagerError.InvalidStatus(status.value) }
             
             // update cached status
-            if self.foundLock.value?.UUID == foundLock.UUID {
-                
-                self.foundLock.value!.status = status.value
-            }
+            lock.status = status.value
             
             return key
         }
         
         /// Unlock the connected lock
-        public func unlock(key: KeyData) throws {
-            
-            guard let foundLock = self.foundLock.value
-                else { throw Error.NoLock }
+        public func unlock(lock: Lock, key: KeyData) throws {
             
             let unlock = LockService.Unlock.init(key: key)
             
-            try internalManager.write(data: unlock.toBigEndian(), response: true, characteristic: LockService.Unlock.UUID, service: LockService.UUID, peripheral: foundLock.peripheral)
+            try internalManager.write(data: unlock.toBigEndian(), response: true, characteristic: LockService.Unlock.UUID, service: LockService.UUID, peripheral: lock.peripheral)
         }
         
-        public func createNewKey(permission: Permission, parentKey: KeyData, sharedSecret: SharedSecret = SharedSecret()) throws {
+        public func createNewKey(lock: inout Lock, permission: Permission, parentKey: KeyData, sharedSecret: SharedSecret = SharedSecret()) throws {
             
             assert(permission != .owner, "Cannot create owner keys")
             
-            guard let foundLock = self.foundLock.value
-                else { throw Error.NoLock }
-            
             let parentNewKey = LockService.NewKeyParentSharedSecret.init(sharedSecret: sharedSecret, parentKey: parentKey, permission: permission)
             
-            try internalManager.write(data: parentNewKey.toBigEndian(), response: true, characteristic: LockService.NewKeyParentSharedSecret.UUID, service: LockService.UUID, peripheral: foundLock.peripheral)
+            try internalManager.write(data: parentNewKey.toBigEndian(), response: true, characteristic: LockService.NewKeyParentSharedSecret.UUID, service: LockService.UUID, peripheral: lock.peripheral)
             
             // update cached status
-            if self.foundLock.value?.UUID == foundLock.UUID {
-                
-                self.foundLock.value!.status = .newKey
-            }
+            lock.status = .newKey
         }
         
-        public func recieveNewKey(sharedSecret: SharedSecret) throws -> Key {
-            
-            guard let foundLock = self.foundLock.value
-                else { throw Error.NoLock }
+        public func recieveNewKey(lock: inout Lock, sharedSecret: SharedSecret) throws -> Key {
             
             // read new key child characteristic
             
-            let newKeyChildValue = try internalManager.read(characteristic: LockService.NewKeyChildSharedSecret.UUID, service: LockService.UUID, peripheral: foundLock.peripheral)
+            let newKeyChildValue = try internalManager.read(characteristic: LockService.NewKeyChildSharedSecret.UUID, service: LockService.UUID, peripheral: lock.peripheral)
             
             guard let newKeyChild = LockService.NewKeyChildSharedSecret.init(bigEndian: newKeyChildValue)
                 else { throw Error.InvalidCharacteristicValue(LockService.NewKeyChildSharedSecret.UUID) }
@@ -173,136 +159,106 @@
             
             let newKeyFinish = LockService.NewKeyFinish.init(key: key.data)
             
-            try internalManager.write(data: newKeyFinish.toBigEndian(), response: true, characteristic: LockService.NewKeyFinish.UUID, service: LockService.UUID, peripheral: foundLock.peripheral)
+            try internalManager.write(data: newKeyFinish.toBigEndian(), response: true, characteristic: LockService.NewKeyFinish.UUID, service: LockService.UUID, peripheral: lock.peripheral)
             
             // update cached status
-            if self.foundLock.value?.UUID == foundLock.UUID {
-                
-                self.foundLock.value!.status = .unlock
-            }
+            lock.status = .unlock
             
             return key
         }
         
         // MARK: - Private Methods
         
-        /// Perform a task on the internal queue.
-        private func async(_ block: () -> ()) {
-            
-            dispatch_async(queue) { block() }
-        }
-        
-        private func stateChanged(state: CBCentralManagerState) {
-            
-            if foundLock.value != nil { foundLock.value = nil }
-            
-            if state == .poweredOn {
-                
-                self.startScan()
-            }
-            
-            self.state.value = state
-        }
-        
-        private func didDisconnect(peripheral: Peripheral) {
-            
-            guard let foundLock = self.foundLock.value else { return }
-            
-            if peripheral.identifier == foundLock.peripheral.identifier {
-                
-                self.foundLock.value = nil
-                
-                LockManager.shared.startScan()
-            }
-        }
-        
-        private func foundLock(peripheral: Peripheral) {
+        private func foundLock(peripheral: Peripheral) throws -> Lock {
             
             log?("Found lock peripheral \(peripheral.identifier)")
             
-            func foundLockError(_ error: String) {
-                
-                log?(error)
-                
-                startScan()
-            }
+            // get lock status
             
-            async { [weak self] in
-                
-                guard let controller = self else { return }
-                
-                do {
-                    
-                    // get lock status
-                    
-                    let characteristics = try controller.internalManager.discoverCharacteristics(for: LockService.UUID, peripheral: peripheral)
-                    
-                    guard characteristics.contains({ $0.UUID == LockService.Status.UUID })
-                        else { foundLockError("Status characteristic not found"); return }
-                    
-                    let statusValue = try controller.internalManager.read(characteristic: LockService.Status.UUID, service: LockService.UUID, peripheral: peripheral)
-                    
-                    guard let status = LockService.Status.init(bigEndian: statusValue)
-                        else { foundLockError("Invalid data for Lock status"); return }
-                    
-                    // get lock UUID
-                    
-                    guard characteristics.contains({ $0.UUID == LockService.Identifier.UUID })
-                        else { foundLockError("Identifier characteristic not found"); return }
-                    
-                    let identifierValue = try controller.internalManager.read(characteristic: LockService.Identifier.UUID, service: LockService.UUID, peripheral: peripheral)
-                    
-                    guard let identifier = LockService.Identifier.init(bigEndian: identifierValue)
-                        else { foundLockError("Invalid data for Lock identifier"); return }
-                    
-                    // get model
-                    
-                    let modelValue = try controller.internalManager.read(characteristic: LockService.Model.UUID, service: LockService.UUID, peripheral: peripheral)
-                    
-                    guard let model = LockService.Model.init(bigEndian: modelValue)
-                        else { foundLockError("Invalid Model value"); return }
-                    
-                    // get version
-                    
-                    let versionValue = try controller.internalManager.read(characteristic: LockService.Version.UUID, service: LockService.UUID, peripheral: peripheral)
-                    
-                    guard let version = LockService.Version.init(bigEndian: versionValue)
-                        else { foundLockError("Invalid Version value"); return }
-                    
-                    // validate other characteristics
-                    
-                    guard characteristics.contains({ $0.UUID == LockService.Setup.UUID })
-                        else { foundLockError("Setup characteristic not found"); return }
-                    
-                    guard characteristics.contains({ $0.UUID == LockService.Unlock.UUID })
-                        else { foundLockError("Unlock characteristic not found"); return }
-                    
-                    guard characteristics.contains({ $0.UUID == LockService.NewKeyParentSharedSecret.UUID })
-                        else { foundLockError("New Key Parent characteristic not found"); return }
-                    
-                    guard characteristics.contains({ $0.UUID == LockService.NewKeyChildSharedSecret.UUID })
-                        else { foundLockError("New Key Child characteristic not found"); return }
-                    
-                    guard characteristics.contains({ $0.UUID == LockService.NewKeyFinish.UUID })
-                        else { foundLockError("New Key Confirmation characteristic not found"); return }
-                    
-                    controller.log?("Lock \((peripheral, identifier.value, status.value, model.value, version.value))")
-                    
-                    controller.foundLock.value = (peripheral, identifier.value, status.value, model.value, version.value)
-                }
-                    
-                catch { foundLockError("\(error)"); return }
-            }
+            let characteristics = try internalManager.discoverCharacteristics(for: LockService.UUID, peripheral: peripheral)
+            
+            guard characteristics.contains({ $0.UUID == LockService.Status.UUID })
+                else { throw Error.CharacteristicNotFound(LockService.Status.UUID) }
+            
+            let statusValue = try internalManager.read(characteristic: LockService.Status.UUID, service: LockService.UUID, peripheral: peripheral)
+            
+            guard let status = LockService.Status.init(bigEndian: statusValue)
+                else { throw Error.InvalidCharacteristicValue(LockService.Status.UUID) }
+            
+            // get lock UUID
+            
+            guard characteristics.contains({ $0.UUID == LockService.Identifier.UUID })
+                else { throw Error.CharacteristicNotFound(LockService.Identifier.UUID) }
+            
+            let identifierValue = try internalManager.read(characteristic: LockService.Identifier.UUID, service: LockService.UUID, peripheral: peripheral)
+            
+            guard let identifier = LockService.Identifier.init(bigEndian: identifierValue)
+                else { throw Error.InvalidCharacteristicValue(LockService.Identifier.UUID) }
+            
+            // get model
+            
+            let modelValue = try internalManager.read(characteristic: LockService.Model.UUID, service: LockService.UUID, peripheral: peripheral)
+            
+            guard let model = LockService.Model.init(bigEndian: modelValue)
+                else { throw Error.InvalidCharacteristicValue(LockService.Model.UUID) }
+            
+            // get version
+            
+            let versionValue = try internalManager.read(characteristic: LockService.Version.UUID, service: LockService.UUID, peripheral: peripheral)
+            
+            guard let version = LockService.Version.init(bigEndian: versionValue)
+                else { throw Error.InvalidCharacteristicValue(LockService.Version.UUID) }
+            
+            // validate other characteristics
+            
+            guard characteristics.contains({ $0.UUID == LockService.Setup.UUID })
+                else { throw Error.CharacteristicNotFound(LockService.Setup.UUID) }
+            
+            guard characteristics.contains({ $0.UUID == LockService.Unlock.UUID })
+                else { throw Error.CharacteristicNotFound(LockService.Unlock.UUID) }
+            
+            guard characteristics.contains({ $0.UUID == LockService.NewKeyParentSharedSecret.UUID })
+                else { throw Error.CharacteristicNotFound(LockService.NewKeyParentSharedSecret.UUID) }
+            
+            guard characteristics.contains({ $0.UUID == LockService.NewKeyChildSharedSecret.UUID })
+                else { throw Error.CharacteristicNotFound(LockService.NewKeyChildSharedSecret.UUID) }
+            
+            guard characteristics.contains({ $0.UUID == LockService.NewKeyFinish.UUID })
+                else { throw Error.CharacteristicNotFound(LockService.NewKeyFinish.UUID) }
+            
+            log?("Lock \((peripheral, identifier.value, status.value, model.value, version.value))")
+            
+            return Lock(peripheral: peripheral, UUID: identifier.value, status: status.value, model: model.value, version: version.value)
         }
     }
     
     public enum LockManagerError: ErrorProtocol {
         
-        case NoLock
         case InvalidStatus(Status)
         case InvalidSharedSecret
         case CharacteristicNotFound(Bluetooth.UUID)
         case InvalidCharacteristicValue(Bluetooth.UUID)
+    }
+    
+    public extension LockManager {
+        
+        public struct Lock {
+            
+            public let peripheral: Peripheral
+            public let UUID: SwiftFoundation.UUID
+            public let model: Model
+            public let version: UInt64
+            public var status: Status
+            
+            private init(peripheral: Peripheral, UUID: SwiftFoundation.UUID, status: Status, model: Model, version: UInt64) {
+                
+                self.peripheral = peripheral
+                self.UUID = UUID
+                self.status = status
+                self.model = model
+                self.version = version
+            }
+        }
     }
     
 #endif
