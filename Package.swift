5261d4d404931c5617773518404cae244b5f708d
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-25 07:38:19 -0500

    Fixed Cb64 URL

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PureSwift/GATT.git", majorVersion: 1),
         .Package(url: "https://github.com/ColemanCDA/CryptoSwift", majorVersion: 1),
-        .Package(url: "https://github.com/ColemanCDA/Cb64", majorVersion: 1)
+        .Package(url: "https://github.com/PureSwift/Cb64", majorVersion: 1)
     ],
     targets: [
         Target(
